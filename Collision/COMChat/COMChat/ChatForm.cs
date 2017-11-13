9b1343020de2f4ec1184ed501ddbdea1ce24a035
Unknown <antoha54785485@mail.ru>
2017-11-13 22:52:43 +0300

    ComChat with collision and jam.

---
+++ Collision/COMChat/COMChat/ChatForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace COMChat
+{
+    public partial class ChatForm : Form
+    {
+        private readonly PortController _serialPort;
+
+        public ChatForm()
+        {
+            InitializeComponent();
+            _serialPort = new PortController(ShowData);
+        }
+
+        private void CanConnect(object sender, EventArgs e)
+        {
+            button1.Enabled = textBox1.Text.Length != 0 && (comboBox1.SelectedIndex > -1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (button1.Text)
+            {
+                case "Connect": Connect(); break;
+                case "Disconnect": Disconnect(); break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _serialPort.SendData(Encoding.Default.GetBytes(richTextBox2.Text));
+            ShowData(richTextBox2.Text, true);
+            richTextBox2.Text = "";
+        }
+
+        private void ShowData(string data, bool sender)
+        {
+            if (!InvokeRequired)
+            {
+                richTextBox1.Text += (sender ? "↑" : "↓") + @" [" + DateTime.Now.ToString("HH:mm:ss tt") + @"]: " + data + "\n";
+            }
+            else
+            {
+                Invoke(new Action<string, bool>(ShowData), data, sender);
+            }
+        }
+
+        private void Connect()
+        {
+            _serialPort.Connect(textBox1.Text, Int32.Parse(comboBox1.SelectedItem.ToString()));
+            button1.Text = @"Disconnect";
+            textBox1.Enabled = false;
+            comboBox1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void Disconnect()
+        {
+            _serialPort.Disconnect();
+            button1.Text = @"Connect";
+            textBox1.Enabled = true;
+            comboBox1.Enabled = true;
+            button2.Enabled = false;
+        }
+    }
+}
---
+++ Collision/COMChat/COMChat/PortController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+
+namespace COMChat
+{
+    class PortController
+    {
+        private SerialPort _serialPort;
+        public delegate void ShowDataHandler(string data, bool sender);
+        private readonly ShowDataHandler _handler;
+        private const int Attempts = 10;
+        private const byte JamByte = 0x8C;
+        private const byte EndByte = 0x8E;
+        private const int CollisionWindowTimeout = 10;
+        private const int SlotTimeLength = 30;
+
+        public PortController(ShowDataHandler handler)
+        {
+            _handler = handler;
+        }
+
+        public void Connect(string portName, int baudRate)
+        {
+            _serialPort = new SerialPort(portName, baudRate, Parity.None, 8, StopBits.One);
+            _serialPort.Open();
+            _serialPort.DataReceived += DataReceived;
+        }
+
+        public void Disconnect()
+        {
+            _serialPort.Close();
+        }
+
+        public void SendData(byte[] data)
+        {
+            foreach (var _byte in data)
+            {
+                SendByte(_byte);
+            }
+            _serialPort.Write(new[] { EndByte }, 0, 1);
+        }
+
+        private void SendByte(byte _byte)
+        {
+            int i = 0;
+            while(true)
+            {
+                while (!IsChannelFreeOrCollisionIs()) { }
+                _serialPort.Write(new[] { _byte }, 0, 1);
+                Thread.Sleep(CollisionWindowTimeout);
+                if (!IsChannelFreeOrCollisionIs())
+                {
+                    return;
+                }
+                _serialPort.Write(new[] { JamByte }, 0, 1);
+#if DEBUG
+                Console.WriteLine(JamByte);
+#endif
+                if (++i == Attempts)
+                    throw new Exception("Error!!!");
+                Thread.Sleep(new Random().Next(0, (int)Math.Pow(2, Math.Min(i, 10))) * SlotTimeLength);
+            }
+            
+        }
+
+        private void DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var _message = new List<byte>();
+            while (true)
+            {
+                if (_serialPort.BytesToRead == 0)
+                {
+                    continue;
+                }
+                var _byte = (byte)_serialPort.ReadByte();
+                if (_byte == EndByte)
+                {
+                    break;
+                }
+                if (_byte == JamByte)
+                {
+                    _message.RemoveAt(_message.Count - 1);
+                }
+                else
+                {
+                    _message.Add(_byte);
+                }
+            }
+            _handler.Invoke(System.Text.Encoding.Default.GetString(_message.ToArray()), false);
+        }
+
+        private bool IsChannelFreeOrCollisionIs()
+        {
+            return DateTime.Now.TimeOfDay.Milliseconds % 2 == 0;
+        }
+    }
+}
